dbcafd089f1ea4f4d711d50b1017f501106bf8e8
Geo Mircean <geomircen@gmail.com>
2018-11-05 14:35:59 +0200

    Updates

---
+++ precache-manifest.8342bbc8627c447be47219babea01262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "280e985e4c7b413d5051",
+    "url": "/init-funnel-track/static/js/runtime~main.280e985e.js"
+  },
+  {
+    "revision": "d79a320cf3fca236dd5b",
+    "url": "/init-funnel-track/static/js/main.d79a320c.chunk.js"
+  },
+  {
+    "revision": "6eac8dc5d4f1558e15aa",
+    "url": "/init-funnel-track/static/js/1.6eac8dc5.chunk.js"
+  },
+  {
+    "revision": "d79a320cf3fca236dd5b",
+    "url": "/init-funnel-track/static/css/main.76d7ce2a.chunk.css"
+  },
+  {
+    "revision": "8318be3c544f94ffb2e87b6e1b3c850e",
+    "url": "/init-funnel-track/index.html"
+  }
+];
