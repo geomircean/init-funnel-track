f2d267207c24de010872d37a2c79692f3af12dc5
geomircean <geomircean@gmail.com>
2018-11-06 14:54:36 +0200

    Add script load on each page enter

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import NavMenu from './components/Menu';
 import { Home, About, ThankYou, Cart, Checkout } from './pages';
+import loadTrackingScript from './tracking-script';
 import createHistory from 'history/createBrowserHistory';
 
 const AppRoutes = () => {
@@ -10,11 +11,11 @@ const AppRoutes = () => {
 
       <div>
         <NavMenu/>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/thank-you" exact component={ThankYou} />
-        <Route path="/checkout" exact component={Checkout} />
-        <Route path="/cart" exact component={Cart} />
+        <Route path="/" exact component={Home} onEnter={loadTrackingScript}/>
+        <Route path="/about" exact component={About} onEnter={loadTrackingScript} />
+        <Route path="/thank-you" exact component={ThankYou} onEnter={loadTrackingScript} />
+        <Route path="/checkout" exact component={Checkout} onEnter={loadTrackingScript} />
+        <Route path="/cart" exact component={Cart} onEnter={loadTrackingScript}/>
         {/*<Route component={NoMatch} />*/}
       </div>
 
