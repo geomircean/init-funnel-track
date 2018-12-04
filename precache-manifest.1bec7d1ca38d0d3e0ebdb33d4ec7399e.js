b392fbbbece20de1f3d8a3a6609dbe6f9ca9064e
Victor Toma <victor.toma@evozon.com>
2018-12-04 12:20:31 +0200

    Updates

--- precache-manifest.1bec7d1ca38d0d3e0ebdb33d4ec7399e.js
+++ precache-manifest.1bec7d1ca38d0d3e0ebdb33d4ec7399e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/init-funnel-track/static/js/runtime~main.280e985e.js"
   },
   {
-    "revision": "f38101ebca61b7ce8796",
-    "url": "/init-funnel-track/static/js/main.f38101eb.chunk.js"
+    "revision": "560f25d77d79510d539f",
+    "url": "/init-funnel-track/static/js/main.560f25d7.chunk.js"
   },
   {
     "revision": "4df15e77ee59928e4ab9",
     "url": "/init-funnel-track/static/js/1.4df15e77.chunk.js"
   },
   {
-    "revision": "f38101ebca61b7ce8796",
+    "revision": "560f25d77d79510d539f",
     "url": "/init-funnel-track/static/css/main.b13ebe90.chunk.css"
   },
   {
-    "revision": "389b8d7457495c274fbee9194c1ec2af",
+    "revision": "3220bf9c774fb61283ea4b3305a71145",
     "url": "/init-funnel-track/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/init-funnel-track/precache-manifest.1b0c02b841293dbb278f05ae8adb5316.js"
+  "/init-funnel-track/precache-manifest.1bec7d1ca38d0d3e0ebdb33d4ec7399e.js"
 );
 
 workbox.clientsClaim();
