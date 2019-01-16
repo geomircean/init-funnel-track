b722b9e5d9fefc9fe81cc58e5c3c9ef4a2358ace
Victor Toma <victor.toma@evozon.com>
2019-01-16 13:44:59 +0200

    Updates

--- precache-manifest.dfe837a3791f04d6d9b5a7e8ea2946bb.js
+++ precache-manifest.dfe837a3791f04d6d9b5a7e8ea2946bb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/init-funnel-track/static/js/runtime~main.280e985e.js"
   },
   {
-    "revision": "164e088742aacc4d2ba1",
-    "url": "/init-funnel-track/static/js/main.164e0887.chunk.js"
+    "revision": "e26b89894903afcda337",
+    "url": "/init-funnel-track/static/js/main.e26b8989.chunk.js"
   },
   {
     "revision": "d1fc4d15029bbaeaa306",
     "url": "/init-funnel-track/static/js/1.d1fc4d15.chunk.js"
   },
   {
-    "revision": "164e088742aacc4d2ba1",
+    "revision": "e26b89894903afcda337",
     "url": "/init-funnel-track/static/css/main.44723f6a.chunk.css"
   },
   {
-    "revision": "265b1907b668942524a5b896bc24aec8",
+    "revision": "c6b5865b511aa1b7e7a4a78b301d6545",
     "url": "/init-funnel-track/index.html"
   }
 ];
