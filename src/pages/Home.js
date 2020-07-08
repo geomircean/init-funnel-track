d68488a5356b982a6ac342b3d9dc67872b6b31e8
Geo Mircean <geomircean@gmail.com>
2020-07-08 17:33:07 +0300

    Test iframing site

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Container from '../components/Container';
 const Home = () => (
   <Container>
     <h2>Home</h2>
+    <iframe src="https://immqa3.devnest.ro/dashboard/"/>
   </Container>
 );
 
