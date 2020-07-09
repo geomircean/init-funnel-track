b0fdbf8ec410584a3fd92c9a40e7d8c9bdd173e0
Geo Mircean <geomircean@gmail.com>
2020-07-09 10:51:15 +0300

    Updates

--- precache-manifest.ff504a45b95be103962f7e1d7ebd6307.js
+++ precache-manifest.ff504a45b95be103962f7e1d7ebd6307.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff4ae369742f48c6c2488fb047b081b1",
+    "revision": "7092a95851c46bc57eb3f4f68f44ac4c",
     "url": "/init-funnel-track/index.html"
   },
   {
-    "revision": "777073b51dedc1393313",
+    "revision": "fc4a6abf980209a9f836",
     "url": "/init-funnel-track/static/css/main.c883c3a2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/init-funnel-track/static/js/2.ee504322.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "777073b51dedc1393313",
-    "url": "/init-funnel-track/static/js/main.71cfbfb1.chunk.js"
+    "revision": "fc4a6abf980209a9f836",
+    "url": "/init-funnel-track/static/js/main.b999d401.chunk.js"
   },
   {
     "revision": "541bcc24e5c3bfb5cb85",
