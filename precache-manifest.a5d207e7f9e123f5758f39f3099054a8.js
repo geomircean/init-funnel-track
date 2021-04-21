7b36d14fc6ae4b113892f4d4de5c38b6340880fb
Geo Mircean <geomircean@gmail.com>
2021-04-21 10:01:06 +0300

    Updates

--- precache-manifest.a5d207e7f9e123f5758f39f3099054a8.js
+++ precache-manifest.a5d207e7f9e123f5758f39f3099054a8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8595ae6d4845ab373c002ff5f567552c",
+    "revision": "d2a3f05befadfff0347138235ab90f33",
     "url": "/init-funnel-track/index.html"
   },
   {
-    "revision": "97109b83a654a48aa5b9",
+    "revision": "496a5e84e322cf4e42b6",
     "url": "/init-funnel-track/static/css/main.cafcd002.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/init-funnel-track/static/js/2.734ca7e6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "97109b83a654a48aa5b9",
-    "url": "/init-funnel-track/static/js/main.b0876e2d.chunk.js"
+    "revision": "496a5e84e322cf4e42b6",
+    "url": "/init-funnel-track/static/js/main.5285000d.chunk.js"
   },
   {
     "revision": "541bcc24e5c3bfb5cb85",
