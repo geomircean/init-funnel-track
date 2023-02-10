7013aebbd7421e2ac356d2a91f782d1822f3241a
Geo Mircean <geomircean@gmail.com>
2023-02-10 16:27:09 +0200

    Embed back qa3

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import loadTrackingScript from '../tracking-script-g000';
+import trackingScriptQA3 from '../tracking-script-immqa3';
 
 class Container extends Component {
   componentDidMount() {
     setTimeout(loadTrackingScript, 10);
+    setTimeout(trackingScriptQA3, 10);
   }
 
   render() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Container from '../components/Container';
-
 const Home = () => (
   <Container>
     <h2>Home</h2>
-    <iframe height={1000} title='My frame' src="https://immqa.devnest.ro"/>
+    {/*<iframe height={1000} title='My frame' src="https://immqa.devnest.ro"/>*/}
   </Container>
 );
 
